8568b8cd9406c2623c0b9f4032bc4d3bf734740f
Nicolás Peñalba <matador50200@gmail.com>
2026-05-11 00:31:10 -0300

    merequeteque

---
+++ CBB/Tp3/Ej1/Tp3Ej1.pas
@@ -0,0 +1,157 @@
+{
+	1. Se cuenta con un archivo que almacena información sobre especies
+	de plantas originarias de Europa, de cada especie se almacena:
+	código especie, nombre vulgar, nombre científico, altura promedio,
+	descripción y zona geográfica. El archivo no está ordenado por
+	ningún criterio. Realice un programa que elimine especies de
+	plantas trepadoras. Para ello se recibe por teclado los códigos
+	de especies a eliminar.
+	
+	a. Implemente una alternativa para borrar especies, que
+	inicialmente marque los registros a borrar y posteriormente
+	compacte el archivo, creando un nuevo archivo sin
+	los registros eliminados.
+	b. Implemente otra alternativa donde para quitar los registros se
+	deberá copiar el último registro del archivo en la posición del
+	registro a borrar y luego eliminar del archivo el último registro
+	de forma tal de evitar registros duplicados.
+	Nota: Las bajas deben finalizar al recibir el código 100000
+}
+{
+Type
+	persona= record
+		ape:st20;
+		nom:st20;
+		fecNac:longint;
+	end;
+	archivo = file of persona;
+Procedure BajaFisica(var a:archivo; apellido,nombre: String);
+	Var
+		posBorrar:integer;
+		rp,aux: per;
+		apellido, nombre: st20;
+	begin
+		reset(a);
+		readln(apellido);
+		readln(nombre);
+		rp.ape= ‘zzz’;
+		rp.nom=‘zzz’;		
+		while (not eof(a) and ((rp.ape <> apellido) and (rp.nom <> nombre))) do
+			read(a,rp);
+		if(rp.ape = apellido) and (rp.nom=nombre)then begin
+			posBorrar:= filepos(a)-1;
+			seek(a,filesize(a)-1);		
+			read(a,aux);
+			seek(a, posBorrar);		
+			write(a,aux);
+			seek(a,filesize(a)-1);
+			truncate(a);
+		end,
+		close(a);
+	end;
+}
+{
+Type
+	persona= record
+		ape:st20;
+		nom:st20;
+		fecNac:longint;
+	end;
+	archivo=file of persona;
+	
+Procedure BajaLogica(var a: archivo; apellido,nombre: String);
+	Var
+		posBorrar:integer;
+		rp:persona;
+		apellido, nombre: st20;
+	begin
+		reset(a);
+		readln(apellido); readln(nombre);
+		rp.ape= ‘zzz’; rp.nom=‘zzz’;
+		while (not eof(a) and ((rp.ape <> apellido) and (rp.nom<>nombre))) do
+			read(a,rp);
+		If(rp.ape = apellido) and (rp.nom=nombre)then begin
+			posBorrar:=filepos(a)-1;
+			rp.ape=“@”
+			seek(a, posBorrar);
+			write(a,rp);
+		end;
+		close(a);
+	end;
+}
+
+program tp3ej1;
+const
+	FIN= 10000;
+	BORRAR= -420;
+	
+type
+	tipoEspecie = record
+		cod: integer;
+		nomVulgar: String[50];
+		nomCientifico: String[50];
+		altPromedio: real;
+		desc: String[50];
+		zona: String[50];
+	end;
+	
+	archEspecie = file of tipoEspecie;
+	
+procedure BajaLogica(var a: archEspecie; cod: integer);
+	var
+		posBorrar: integer;
+		rp: tipoEspecie;
+		
+	begin
+		reset(a);
+		rp.cod:= cod;
+		while (not eof(a)) and (rp.cod <> cod) do
+			read(a, rp);
+		if (rp.cod = cod) then begin
+			posBorrar:= filepos(a)-1;
+			rp.cod:= BORRAR;
+			seek(a, posBorrar);
+			write(a,rp);
+		end;
+		close(a);
+	end;
+	
+procedure BajaFisica(var a: archEspecie; cod: integer);
+	var
+		posBorrar: integer;
+		rp, aux: tipoEspecie;
+	begin
+		reset(a);
+		rp.cod:= cod;
+		while(not eof(a)) do begin
+			while(not eof(a)) and (rp.cod <> cod) do
+				read(a,rp);
+			if (rp.cod = BORRAR) then begin
+				posBorrar:= filepos(a)-1;
+				seek(a, filesize(a)-1);
+				read(a, aux);
+				seek(a, posBorrar);
+				write(a,aux);
+				seek(a, filesize(a)-1);
+				truncate(a);
+			end;
+			rp.cod:= BORRAR;
+		end;
+		close(a);
+	end;
+var
+	codAct: integer;
+	especies: archEspecie;
+BEGIN
+	assign(especies, 'especies.dat');
+	
+	writeln('Ingrese un codigo:');
+	readln(codAct);
+	while (codAct <> FIN) do begin
+		BajaLogica(especies, codAct);
+		writeln('Ingrese otro codigo o ', FIN, ' para terminar:');
+		readln(codAct);
+	end;
+	BajaFisica(especies, BORRAR);
+END.
+
